2f8016912543ab6f2f4b2a07ce3797530f4b6cd5
Kamil Łaskarzewski <k4mlas@gmail.com>
2022-11-28 19:37:15 +0100

    finisch section headerImg

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
@@ -7,3 +8,9 @@ const handleNav = () => {
 };
 
 navBtn.addEventListener('click', handleNav);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
