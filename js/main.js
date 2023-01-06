0c45f5c65c2785ed532607d3deef8d9c4a14d4a2
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-01-06 16:35:05 +0100

    finisch all

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
 
+const arrow = document.querySelector('.header__arrow-icon');
+const textOne = document.querySelector('.header__text-title');
+const span = document.querySelector('.andSpan');
+
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
@@ -13,4 +17,23 @@ const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
+
+white = () => {
+	textOne.style.color = '#e0d7d7';
+};
+black = () => {
+	textOne.style.color = 'black';
+};
+whiteAndOne = () => {
+	span.style.color = 'black';
+};
+whiteAndTwo = () => {
+	span.style.color = '#e0d7d7';
+};
+
+arrow.addEventListener('mouseover', white);
+arrow.addEventListener('mouseleave', black);
+arrow.addEventListener('mouseover', whiteAndOne);
+arrow.addEventListener('mouseleave', whiteAndTwo);
+
 handleCurrentYear();
